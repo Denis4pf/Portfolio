239941467cbb5ac58d8791e6dcaa758ff179d102
Orina <108649085+Denis4pf@users.noreply.github.com>
2023-04-20 20:17:41 +0300

    Create Resume.jsx

---
+++ src/container/Resume.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+function Resume() {
+   
+   return(
+     <button className="button">
+    <a className="button" href="../assets/Orinas.pdf" download = "orinas.pdf">
+      Download Resume
+    </a>
+</button>
+   );
+ };
+
+export default Resume;
