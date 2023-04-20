e9ac0ced0cc369e6602153ed310c13759eb18546
Orina <108649085+Denis4pf@users.noreply.github.com>
2023-04-20 20:52:05 +0300

    Update images.js

--- src/constants/images.js
+++ src/constants/images.js
@@ -2,6 +2,7 @@ import email from "../assets/email.png";
 import mobile from "../assets/mobile.png";
 import api from "../assets/api.png";
 import cpp from "../assets/cpp.png";
+import resume from "../assets/Orinas.pdf;
 import css from "../assets/css.png";
 import figma from "../assets/figma.png";
 import flutter from "../assets/flutter.png";
